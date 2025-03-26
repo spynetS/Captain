6dd28265c762a89b4eb791c8dba72fa433df6728
Leo Modin <mole23@jk.se>
2025-03-26 19:01:58 +0100

    added smooth like butter camera movement

---
+++ Assets/Player/CameraMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPos = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPos, speed * Time.deltaTime);
+    }
+}
