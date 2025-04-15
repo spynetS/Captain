c72bbbb64749b9b4cd10e27ad171428ea4da0530
Alfred <alfred@stensatter.se>
2025-04-15 20:36:22 +0200

    init testing

---
+++ Assets/Resources/Tests/NewTestScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        Assert.AreEqual(true,true);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        Assert.AreEqual(true,true);
+
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
